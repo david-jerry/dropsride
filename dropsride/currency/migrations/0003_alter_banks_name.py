3f58ccdf483fbb371e514613da86a9fb7d6de2b8
Jeremiah David <jeremiahedavid@gmail.com>
2022-10-28 04:50:26 +0100

    fixed banks and cards and address addition

--- dropsride/currency/migrations/0003_alter_banks_name.py
+++ dropsride/currency/migrations/0003_alter_banks_name.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='banks',
             name='name',
-            field=models.CharField(max_length=500, unique=True),
+            field=models.CharField(max_length=500, unique=False),
         ),
     ]
